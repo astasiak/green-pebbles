7c28d01110a98a15f20bcc8a108770135c4c584e
criceto-pigro <andst4@gmail.com>
2013-12-10 21:10:56 +0100

    players sitting

--- client/js.js
+++ client/js.js
@@ -1,11 +1,12 @@
 $( document ).ready(function() {
 
   var room_name = $.url().param("room");
+  var user_name = $.url().param("user");
   if(room_name) {
     $("title").text(room_name);
   }
   var socket = io.connect('http://localhost:8080');
-  socket.emit("register",{'room':room_name});
+  socket.emit("register",{'room':room_name,'user':user_name});
   
   socket.on('check', function (data) {
     var obj = $('#'+data["id"]);
@@ -27,4 +28,27 @@ $( document ).ready(function() {
       socket.emit("check",{id:$(this).attr('id'),dir:"check"});
     }
   });
+  
+  socket.on('sit', function (data) {
+    var position = data["position"];
+    var player = data["player"];
+    var me = data["you"];
+    var slot = $('#'+position);
+    if(player) {
+      slot.text(player).removeClass("empty");
+      if(me) {
+        slot.addClass('me');
+      } else {
+        slot.removeClass('me');
+      }
+    } else {
+      slot.text('sit down').removeClass('me').addClass("empty");
+    }
+  });
+  
+  $('.player').click(function(){
+    socket.emit('sit_request',{position:$(this).attr('id')});
+  });
 });
+
+
--- server/server.js
+++ server/server.js
@@ -6,17 +6,32 @@ var client_rooms = {};
 io.sockets.on('connection', function (socket) {
   socket.on('register', function (data) {
     var room_name = data['room'];
+    var user_name = data['user'];
     if(!rooms[room_name]) {
       rooms[room_name] = [];
     }
     rooms[room_name].push(socket.id);
-    client_rooms[socket.id] = room_name;
+    client_rooms[socket.id] = {'room':room_name, 'user':user_name};
   });
 
   socket.on('check', function (data) {
-    var room_name = client_rooms[socket.id];
+    var room_name = client_rooms[socket.id]['room'];
     for (var i=0; i<rooms[room_name].length; i++) {
-      io.sockets.socket(rooms[room_name][i]).emit('check', data);
+      socketId = rooms[room_name][i];
+      io.sockets.socket(socketId).emit('check', data);
+    };
+  });
+  
+  socket.on('sit_request', function (data) {
+    var room_name = client_rooms[socket.id]['room'];
+    var user_name = client_rooms[socket.id]['user'];
+    for (var i=0; i<rooms[room_name].length; i++) {
+      socketId = rooms[room_name][i];
+      message = {position:data['position'],player:user_name};
+      if(socket.id==socketId) {
+        message['you']=true;
+      }
+      io.sockets.socket(socketId).emit('sit', message);
     };
   });
 });
