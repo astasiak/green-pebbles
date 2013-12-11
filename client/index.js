8a79e47167ebba14b726f5ffdb98477c26f3b1e1
criceto-pigro <andst4@gmail.com>
2013-12-11 22:44:52 +0100

    nice alert on login page

--- client/index.js
+++ client/index.js
@@ -1,11 +1,21 @@
+function alertShow() {
+  $(".bigInput, #enterButton").hide();
+  $("#mainAlert").show();
+}
+function alertHide() {
+  $("#mainAlert").hide();
+  $(".bigInput, #enterButton").show();
+}
+
 $( document ).ready(function() {
   $("#enterButton").click(function() {
     var username = $.trim($("#username").val());
     var room = $.trim($("#room").val());
     if(username=="" || room=="") {
-      alert("Enter username and room name");
+      alertShow();
     } else {
       window.location.assign("game.html?user="+username+"&room="+room);
     }
   });
+  $("#mainAlert").click(alertHide);
 });
