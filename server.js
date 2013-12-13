5dfada6120b41fa9e47f1920a8b18769a4eb5d20
criceto-pigro <andst4@gmail.com>
2013-12-13 23:32:55 +0100

    server.js refactoring

--- server.js
+++ server.js
@@ -5,52 +5,60 @@ var app = connect.createServer(connect.static('client')).listen(port);
 
 var io = require('socket.io').listen(app);
 
-var rooms = {
-  socket_room_map: {},
-  rooms_map: {},
-  register: function(_socketId, _username, _room) {
-    console.log("Register "+_username+" in "+_room);
-    if(!this.rooms_map[_room]) {
-      this.rooms_map[_room] = [];
+function Room(_name) {
+  this.name = _name;
+  this.players = [];
+}
+Room.prototype.emitOthers = function(socketId,msg,data) {
+  for (var i=0; i<this.players.length; i++) {
+    if (this.players[i].socket != socketId ) {
+      io.sockets.socket(this.players[i].socket).emit(msg, data);
     }
-    this.rooms_map[_room].push({username:_username,socketId:_socketId});
-    this.socket_room_map[_socketId] = {username:_username,room:_room};
-  },
-  getPresence: function(_socketId) {
-    var socketData = this.socket_room_map[_socketId];
-    return {username:socketData.username, room:{name:socketData.room, members:this.rooms_map[socketData.room]}};
   }
+}
+Room.prototype.emit = function(msg,data) {this.emitOthers(null,msg,data);}
+
+var rooms = {}; // name->room
+rooms.room = function(_name) {
+  if(!this[_name]) {
+    this[_name] = new Room();
+  }
+  return this[_name];
 };
+
+function Player(_name,_socket,_room) {
+  this.name = _name;
+  this.socket = _socket;
+  this.room = rooms.room(_room);
+  this.room.players.push(this);
+}
+Player.prototype.emit = function(msg,data) {
+  io.sockets.socket(this.socket).emit(msg,data);
+}
+
+var players = {}; // socket->user
 
 io.sockets.on('connection', function (socket) {
   socket.on('register', function (data) {
-    rooms.register(socket.id, data.user, data.room);
+    var player = new Player(data.user,socket.id,data.room);
+    players[socket.id] = player;
   });
 
   socket.on('check', function (data) {
-    var sendTo = rooms.getPresence(socket.id).room.members;
-    for (var i=0; i<sendTo.length; i++) {
-      io.sockets.socket(sendTo[i].socketId).emit('check', data);
-    };
+    var player = players[socket.id];
+    player.room.emit('check', data);
   });
   
   socket.on('sit_request', function (data) {
-    var presence = rooms.getPresence(socket.id);
-    var sendTo = presence.room.members;
-    for (var i=0; i<sendTo.length; i++) {
-      var message = {position: data['position'], player: presence.username};
-      if(socket.id==sendTo[i].socketId) {
-        message['you']=true;
-      }
-      io.sockets.socket(sendTo[i].socketId).emit('sit', message);
-    };
+    var player = players[socket.id];
+    var message = {position: data['position'], player: player.name};
+    player.room.emitOthers(socket.id,'sit',message);
+    message['you'] = true;
+    player.emit('sit',message);
   });
   
   socket.on('chat', function (data) {
-    var presence = rooms.getPresence(socket.id);
-    var sendTo = presence.room.members;
-    for (var i=0; i<sendTo.length; i++) {
-      io.sockets.socket(sendTo[i].socketId).emit('chat', {username:presence.username,text:data.text});
-    }
+    var player = players[socket.id];
+    player.room.emit('chat', {username:player.name,text:data.text});
   });
 });
